5dc462bf909ffc1ffbcc5767a1fd7de540b7afa4
Roman Tymoshenko <tymoshenkorv@gmail.com>
2016-04-08 17:57:25 +0300

    temp

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
 
   #Api definition
-  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
+  namespace :api, defaults: { format: :json },
+                              constraints: { subdomain: 'api' }, path: '/' do
+    scope module: :v1 do
 
+    end
   end
 end
   # The priority is based upon order of creation: first created -> highest priority.
